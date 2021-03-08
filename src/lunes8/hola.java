04070de52fd2e15cedd2cf840d7faf0f22efc647
Pablo <Pablo@DESKTOP-DNHR1A7>
2021-03-08 09:23:21 +0100

    a

--- src/lunes8/hola.java
+++ src/lunes8/hola.java
@@ -4,10 +4,7 @@ public class hola {
 
 	public static void main(String[] args) {
 
-		System.out.println("Soy la rama maestra");
-		System.out.println("Soy la rama 1");
 		
-		System.out.println("prueba rama 1");
 		
 	}
 
