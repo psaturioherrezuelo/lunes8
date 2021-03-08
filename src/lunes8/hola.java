a180acd1a1d0b9103e806786eaab1e26a3fa1c31
Pablo <Pablo@DESKTOP-DNHR1A7>
2021-03-08 09:03:26 +0100

    inicio

---
+++ src/lunes8/hola.java
@@ -0,0 +1,12 @@
+package lunes8;
+
+public class hola {
+
+	public static void main(String[] args) {
+
+		System.out.println("Soy la rama maestra");
+		System.out.println("Soy la rama 1");
+		
+	}
+
+}
